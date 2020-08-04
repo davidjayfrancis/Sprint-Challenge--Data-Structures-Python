91d62db97feead62f13d0d097cd5a12ef0c66070
David Francis <davidjayfrancis@gmail.com>
2020-08-03 22:06:25 -0700

    hacky solution to the ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,38 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.length = 0
+        self.data = []
+        self.counter = 0
 
     def append(self, item):
-        pass
+        if self.length < self.capacity:
+            self.data.append(item)
+            self.length += 1
+            self.counter += 1
+            if self.counter == self.capacity: 
+                self.counter = 0
+        else:
+            self.data[self.counter] = item
+            self.counter += 1
+            if self.counter == self.capacity: 
+                self.counter = 0
+
 
     def get(self):
-        pass
+        return self.data
+
+
+b = RingBuffer(5)
+b.append('a')
+b.append('b')
+b.append('c')
+print(b.get())
+b.append('d')
+print(b.get())
+b.append('e')
+print(b.get())
+b.append('f')
+print(b.get())
+b.append('g')
+print(b.get())
